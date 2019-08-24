6f082a45279d8e6085cdf537c6948c85e1e1f8ba
modeh <modeh@vernegroup.com>
2019-08-24 20:32:27 +0300

    React Hooks Transformation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Validation from './Component/Validation';
 import Char from './Component/Char';
-class App extends Component {
-	state = {
-		userInputName: ''
+
+const App = () => {
+	const [ userInputName, setUserInputName ] = useState('');
+	const changeInputName = (event) => {
+		setUserInputName(event.target.value);
 	};
 
-	changeInputName = (event) => {
-		this.setState({ userInputName: event.target.value });
-	};
-	deleteInputName = (index) => {
-		const text = this.state.userInputName.split('');
+	const deleteInputName = (index) => {
+		const text = userInputName.split('');
 		text.splice(index, 1);
 		const updateText = text.join('');
-		this.setState({ userInputName: updateText });
+		setUserInputName(updateText);
 	};
-
-	render() {
-		const { userInputName } = this.state; // instead of using this.state.userInputName  everytime you can just destructure the state and use userInputName alone
-		// here is a link : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-		const listName = userInputName.split('').map((chars, index) => {
-			return <Char character={chars} key={index} clicked={() => this.deleteInputName(index)} />;
-		});
-		return (
-			<div>
-				<input type="Text" onChange={this.changeInputName} value={userInputName} />
-				<p>{userInputName}</p>
-				<Validation inputLength={userInputName.length} />
-				{listName}
-			</div>
-		);
-	}
-}
+	const listName = userInputName.split('').map((chars, index) => {
+		return <Char character={chars} key={index} clicked={() => deleteInputName(index)} />;
+	});
+	return (
+		<div>
+			<input type="Text" onChange={changeInputName} value={userInputName} />
+			<p>{userInputName}</p>
+			<Validation inputLength={userInputName.length} />
+			{listName}
+		</div>
+	);
+};
 
 export default App;
+
+// class App extends Component {
+// 	state = {
+// 		userInputName: ''
+// 	};
+
+// 	changeInputName = (event) => {
+// 		this.setState({ userInputName: event.target.value });
+// 	};
+// 	deleteInputName = (index) => {
+// 		const text = this.state.userInputName.split('');
+// 		text.splice(index, 1);
+// 		const updateText = text.join('');
+// 		this.setState({ userInputName: updateText });
+// 	};
+
+// 	render() {
+// 		const { userInputName } = this.state; // instead of using this.state.userInputName  everytime you can just destructure the state and use userInputName alone
+// 		// here is a link : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+// 		const listName = userInputName.split('').map((chars, index) => {
+// 			return <Char character={chars} key={index} clicked={() => this.deleteInputName(index)} />;
+// 		});
+// 		return (
+// 			<div>
+// 				<input type="Text" onChange={this.changeInputName} value={userInputName} />
+// 				<p>{userInputName}</p>
+// 				<Validation inputLength={userInputName.length} />
+// 				{listName}
+// 			</div>
+// 		);
+// 	}
+// }
+
+// export default App;
