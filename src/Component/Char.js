77d1bcf36494dade3bcd3efd57ea9d63d9b1b433
modeh <modeh@vernegroup.com>
2019-08-24 04:26:44 +0300

    added object destructuring

--- src/Component/Char.js
+++ src/Component/Char.js
@@ -1,16 +1,17 @@
-import React from "react";
-const char = props => {
-  const style = {
-    display: "inline-block",
-    padding: "16px",
-    margin: "16px",
-    border: "1px solid black",
-    textAlign: "center"
-  };
-  return (
-    <div style={style} onClick={props.clicked}>
-      {props.character}
-    </div>
-  );
+import React from 'react';
+const char = (props) => {
+	const style = {
+		display: 'inline-block',
+		padding: '16px',
+		margin: '16px',
+		border: '1px solid black',
+		textAlign: 'center'
+	};
+	const { clicked, character } = props; // same thing here Object Destructuring, instead of using props.clicked or props.character everytime you can use them with out dot notation from props
+	return (
+		<div style={style} onClick={clicked}>
+			{character}
+		</div>
+	);
 };
 export default char;
--- src/Component/Validation.jsx
+++ src/Component/Validation.jsx
@@ -1,10 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const validation = props => {
-  return (
-    <div>
-      {props.inputLength > 5 ? <p>Text too long</p> : <p>Text too small</p>}
-    </div>
-  );
+const validation = (props) => {
+	//props.inputLength -> we can destructure it but as long as it is one prop no need to do it, but when you have many props its prefered to destructure so its more flixebale
+	return <div>{props.inputLength > 5 ? <p>Text too long</p> : <p>Text too small</p>}</div>;
 };
 export default validation;
